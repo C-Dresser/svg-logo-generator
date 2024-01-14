0b64687b5ddb650e2c6c0a2431ba3d0455785eea
C-Dresser <142123153+C-Dresser@users.noreply.github.com>
2024-01-14 14:05:43 -0500

    added tests for other shapes

--- classes/shapes.test.js
+++ classes/shapes.test.js
@@ -1,7 +1,8 @@
 // Jest tests for shapes
 //fixed file path for require
 //added test for logo colors
-//added test for logo shape
+//added test for svg generation
+//added test for other shapes
 const { Triangle, Circle, Square } = require('../lib/shapes');
 
 describe('Shape class', () => {
@@ -17,6 +18,20 @@ describe('Shape class', () => {
         triangle.setColor('green', 'yellow');
         const svg = triangle.render(); 
         expect(svg).toContain('<polygon points="100, 18 194, 182 6, 182" fill="green" />');
+      });
+
+      test('render method returns SVG for Circle', () => {
+        const circle = new Circle();
+        circle.setColor('green', 'yellow');
+        const svg = circle.render();
+        expect(svg).toContain('<circle cx="100" cy="100" r="100" fill="green"/>');
+      });
+    
+      test('render method returns SVG for Square', () => {
+        const square = new Square();
+        square.setColor('green', 'yellow');
+        const svg = square.render();
+        expect(svg).toContain('<rect x="50" y="50" height="100" width="100" fill="green"/>');
       });
 
 });
