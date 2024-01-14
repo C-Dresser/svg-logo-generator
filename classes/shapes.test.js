5b9264ae563efd67ab65a02b87a3f1856fd971fc
C-Dresser <142123153+C-Dresser@users.noreply.github.com>
2024-01-14 13:58:20 -0500

    fixed file structure for require in test file and added test for logo colors

--- classes/shapes.test.js
+++ classes/shapes.test.js
@@ -1,2 +1,12 @@
 // Jest tests for shapes
-const { Triangle, Circle, Square } = require('./shapes.js');
+const { Triangle, Circle, Square } = require('../lib/shapes');
+
+describe('Shape class', () => {
+    test('setColor method sets shapeColor and textColor', () => {
+      const shape = new Triangle();
+      shape.setColor('red', 'blue');
+      expect(shape.shapeColor).toBe('red');
+      expect(shape.textColor).toBe('blue');
+    });
+
+});
